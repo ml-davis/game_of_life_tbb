e80c74b2970a96a9dbdeb2028383bd4a5be96b62
Matthew Davis <ml.davis195@gmail.com>
2016-10-14 12:21:09 -0400

    Added opengl functionality and got it working in Linux

--- main.cpp
+++ main.cpp
@@ -1,6 +1,72 @@
+#include <GL/glew.h>
+#include <GL/freeglut.h>
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int pixel_width = 1024;
+int pixel_height = 768;
+
+int cell_size = 8;
+
+int horiz_num_cells = pixel_width / cell_size;
+int vert_num_cells = pixel_height / cell_size;
+
+void set_color(char species) {
+    switch(species) {
+        case 'A':	glColor3f(1.0f, 0.0f, 0.0f); break; // RED
+        case 'B':	glColor3f(0.0f, 1.0f, 0.0f); break; // GREEN
+        case 'C':	glColor3f(0.1f, 0.2f, 1.0f); break; // BLUE
+        case 'D':	glColor3f(1.0f, 1.0f, 0.0f); break; // YELLOW
+        case 'E':	glColor3f(1.0f, 0.0f, 1.0f); break; // MAGENTA
+        case 'F':	glColor3f(0.0f, 1.0f, 1.0f); break; // CYAN
+        case 'G':	glColor3f(1.0f, 1.0f, 1.0f); break; // WHITE
+        case 'H':	glColor3f(1.0f, 0.5f, 0.0f); break; // ORANGE
+        case 'I':	glColor3f(0.5f, 0.5f, 0.5f); break; // GREY
+        case 'J':	glColor3f(0.4f, 0.0f, 1.0f); break; // VIOLET
+        default:	glColor3f(0.0f, 0.0f, 0.0f);		// BLACK
+    }
+}
+
+// colors a square at coordinates x,y a given color
+void paint_cell(float x, float y, char species) {
+    if (x < 0 || x >= horiz_num_cells || y < 0 || y >= vert_num_cells) {
+        std::cout << "Tried to place cell in invalid position: (" << x << ", " << y << ")" << std::endl;
+        std::cin.get();
+        // exit(1); why the fuck won't this work :|
+    }
+
+    x *= cell_size;
+    y *= cell_size;
+
+    set_color(species);
+
+    glBegin(GL_POLYGON);
+    glVertex3f(x,				y,				0.0f);
+    glVertex3f(x + cell_size,	y,				0.0f);
+    glVertex3f(x + cell_size,	y + cell_size,	0.0f);
+    glVertex3f(x,				y + cell_size,	0.0f);
+    glEnd();
+    glFlush();
+}
+
+void display() {
+    char species = 'A';
+    for (int i = 0; i < 10; i++) {
+        paint_cell(i, i, species++);
+    }
+}
+
+int main(int argc, char **argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+
+    glutInitWindowSize(pixel_width, pixel_height);
+    glutCreateWindow("Game of Life 2.1");
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0.0f, pixel_width, pixel_height, 0.0f, -1.0f, 1.0f);
+
+    glutDisplayFunc(display);
+
+    glutMainLoop();
 }
